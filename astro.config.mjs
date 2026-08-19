461f89acb641e6d9c919a7b6cc98e3f7e731d74c
Msaad022 <mohamedsad7090@gmail.com>
2026-08-19 16:20:47 +0300

    deploy commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,8 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
+    site: 'https://Msaad022.github.io',
+    base: '/miras',
   }
 });
